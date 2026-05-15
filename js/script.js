6cd6f49b335620dc3cbcaa79b91bde6700256d2f
Your Name <ju.quesada28@gmail.com>
2026-05-14 22:13:34 -0300

    Mudança completa no esqueleto do site

--- js/script.js
+++ js/script.js
@@ -1,19 +1,56 @@
 const explorarBtn = document.getElementById('explorarBtn');
 
-explorarBtn.addEventListener('click', () => {
-  const catalogo = document.getElementById('catalogo');
+if(explorarBtn){
 
-  catalogo.scrollIntoView({
-    behavior: 'smooth'
+  explorarBtn.addEventListener('click', () => {
+
+    window.location.href = 'produtos.html';
+
   });
-});
+
+}
 
 const formContato = document.getElementById('formContato');
 
-formContato.addEventListener('submit', (e) => {
-  e.preventDefault();
+if(formContato){
 
-  alert('Mensagem enviada com sucesso para a Aoro Joias ✨');
+  formContato.addEventListener('submit', (e) => {
 
-  formContato.reset();
+    e.preventDefault();
+
+    alert('Mensagem enviada com sucesso para a Aoro Joias ✨');
+
+    formContato.reset();
+
+  });
+
+}
+
+/* ANIMAÇÃO DOS CARDS */
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+
+  card.style.opacity = '0';
+  card.style.transform = 'translateY(40px)';
+  card.style.transition = '0.7s';
+
+});
+
+window.addEventListener('scroll', () => {
+
+  cards.forEach(card => {
+
+    const cardTop = card.getBoundingClientRect().top;
+
+    if(cardTop < window.innerHeight - 100){
+
+      card.style.opacity = '1';
+      card.style.transform = 'translateY(0)';
+
+    }
+
+  });
+
 });
